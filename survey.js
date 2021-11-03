0fe75df14c1f954287c6a3eff0f04e86236fd86c
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-02 21:23:10 -0600

    profilegen

--- survey.js
+++ survey.js
@@ -1,11 +1,11 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
-let variable = ''; 
-rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => {
+let variable = "";
+rl.question("What's your name? Nicknames are also acceptable :) ", (answer) => {
   // TODO: Log the answer in a database
   // console.log(`Thank you for your valuable feedback: ${answer}`),
   variable += "  " + answer;
@@ -13,41 +13,48 @@ rl.question('What\'s your name? Nicknames are also acceptable :) ', (answer) => 
     // TODO: Log the answer in a database
     variable += " loves listening to " + answer;
     // console.log(`Thank you for your valuable feedback: ${variable}`)
-    rl.question('What\'s an activity you like doing?', (answer) => {
+    rl.question("What's an activity you like doing?", (answer) => {
       // TODO: Log the answer in a database
       variable += " while " + answer;
       // console.log(`Thank you for your valuable feedback: ${variable}`)
-      
-      rl.question('What\'s your favourite thing to eat for that meal? ', (answer) => {
-        // TODO: Log the answer in a database
-        variable += ", devouring "+ answer;
-        // console.log(`Thank you for your valuable feedback: ${variable}`)
-        rl.question('Which meal is your favourite (eg: dinner, brunch, etc.) ', (answer) => {
-          // TODO: Log the answer in a database
-          variable += " for " + answer;
-          // console.log(`Thank you for your valuable feedback: ${variable}`)
-          
-          rl.question('Which sport is your absolute favourite? ', (answer) => {
-            // TODO: Log the answer in a database
-            variable += ", prefers " + answer;
-            // console.log(`Thank you for your valuable feedback: ${variable}`)
-            rl.question('What is your superpower? In a few words, tell us what you are amazing at! ', (answer) => {
-              // TODO: Log the answer in a database
-              variable += " over any other sport, and is amazing at " + answer;
-              console.log(`Thank you for your valuable feedback: ${variable}`)
-              rl.close();
-              
-            });
-            
-          });
-        });
-        
-      });
-      
-    });
-    
-  });
-  
 
-  
+      rl.question(
+        "What's your favourite thing to eat for that meal? ",
+        (answer) => {
+          // TODO: Log the answer in a database
+          variable += ", devouring " + answer;
+          // console.log(`Thank you for your valuable feedback: ${variable}`)
+          rl.question(
+            "Which meal is your favourite (eg: dinner, brunch, etc.) ",
+            (answer) => {
+              // TODO: Log the answer in a database
+              variable += " for " + answer;
+              // console.log(`Thank you for your valuable feedback: ${variable}`)
+
+              rl.question(
+                "Which sport is your absolute favourite? ",
+                (answer) => {
+                  // TODO: Log the answer in a database
+                  variable += ", prefers " + answer;
+                  // console.log(`Thank you for your valuable feedback: ${variable}`)
+                  rl.question(
+                    "What is your superpower? In a few words, tell us what you are amazing at! ",
+                    (answer) => {
+                      // TODO: Log the answer in a database
+                      variable +=
+                        " over any other sport, and is amazing at " + answer;
+                      console.log(
+                        `Thank you for your valuable feedback: ${variable}`
+                      );
+                      rl.close();
+                    }
+                  );
+                }
+              );
+            }
+          );
+        }
+      );
+    });
+  });
 });
